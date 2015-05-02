5c3d03d2984d97dd298039f1f0f925a7a0dc83a8
Lucas A Jones <Necxii@gmail.com>
2015-05-02 17:57:11 +0100

    Merge

--- src/scenes/BasicScene.hx
+++ src/scenes/BasicScene.hx
@@ -1,20 +1,24 @@
 package scenes;
 
 import components.EntityComponent;
+import differ.Collision;
 import milkshake.core.Graphics;
-import milkshake.core.Sprite;
 import milkshake.game.scene.camera.CameraPresets;
 import milkshake.game.scene.Scene;
 import milkshake.game.tile.renderers.BoltTileMapRenderer;
 import milkshake.game.tile.TileMap;
+import milkshake.game.tile.TileMapCollision;
 import milkshake.game.tile.TileMapData;
 import milkshake.utils.Color;
 import pixi.BaseTexture;
-import pixi.Rectangle;
+
 
 class BasicScene extends Scene
 {
-	private var entityComponent:EntityComponent;
+	var entityComponent:EntityComponent;		
+	var tilemapCollision(default, null):TileMapCollision;
+
+	var graphics:Graphics;
 
 	public function new()
 	{
@@ -26,20 +30,48 @@ class BasicScene extends Scene
 		super.create();
 
 		var tileMapData = TileMapData.fromCSV(CompileTime.readFile("assets/tilemaps/main.csv"));
-		var tilemap = new TileMap(tileMapData, new BoltTileMapRenderer(BaseTexture.fromImage("assets/tilesheets/main.png"), 64, true, false));
 
-		addNode(tilemap);
+		addNode(new TileMap(tileMapData, new BoltTileMapRenderer(BaseTexture.fromImage("assets/tilesheets/main.png"), 64, true, false)));
+		addNode(tilemapCollision = new TileMapCollision(tileMapData, 64));
 
-		var player = Sprite.fromUrl("assets/sprites/player.png");
+		addNode(entityComponent = new EntityComponent());
 
-		addNode(player);
-		
-		entityComponent = new EntityComponent();
-		addNode(entityComponent);
+		// Debug		
+		addNode(graphics = new Graphics());
 	}
 
 	override public function update(deltaTime:Float):Void
 	{
 		super.update(deltaTime);
+
+		debugRender();
+
+		var results = Collision.shapeWithShapes(box.body, cast tilemapCollision.rectangles);
+
+		for(result in results)
+		{
+			box.body.x += result.separation.x;
+			box.body.y += result.separation.y;
+		}
+	}
+
+	function debugRender()
+	{
+		graphics.clear();
+		graphics.begin(Color.RED, 0.1, 1, Color.RED);
+
+		for(rectangle in tilemapCollision.rectangles)
+		{
+			for(index in 0 ... rectangle.transformedVertices.length)
+			{
+				var pointA = rectangle.transformedVertices[index];
+				var pointB = index + 1 < rectangle.transformedVertices.length ? rectangle.transformedVertices[index + 1] : rectangle.transformedVertices[0];
+
+				graphics.graphics.moveTo(pointA.x, pointA.y);
+				graphics.graphics.lineTo(pointB.x, pointB.y);
+			}
+		}
+
+		graphics.graphics.drawRect(box.body.x, box.body.y, 64, 64);
 	}
 }
