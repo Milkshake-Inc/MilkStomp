b950843de4307ac8a8a4f3447c23cc22c119524d
Lucas A Jones <Necxii@gmail.com>
2015-05-02 16:46:19 +0100

    Added tilemap & player test

--- src/scenes/BasicScene.hx
+++ src/scenes/BasicScene.hx
@@ -1,27 +1,35 @@
 package scenes;
 
 import milkshake.core.Graphics;
+import milkshake.core.Sprite;
 import milkshake.game.scene.camera.CameraPresets;
 import milkshake.game.scene.Scene;
+import milkshake.game.tile.renderers.BoltTileMapRenderer;
+import milkshake.game.tile.TileMap;
+import milkshake.game.tile.TileMapData;
 import milkshake.utils.Color;
+import pixi.BaseTexture;
 import pixi.Rectangle;
 
 class BasicScene extends Scene
 {
 	public function new()
 	{
-		super("BasicScene", [ ], CameraPresets.DEFAULT, Color.BLUE);
+		super("BasicScene", [ "assets/tilesheets/main.png" ], CameraPresets.DEFAULT, Color.BLUE);
 	}
 
 	override public function create():Void
 	{
 		super.create();
 
-		var graphics = new Graphics();
-		graphics.begin(0xFF0000);
-		graphics.drawRectangle(new Rectangle(0, 0, 100, 100));
+		var tileMapData = TileMapData.fromCSV(CompileTime.readFile("assets/tilemaps/main.csv"));
+		var tilemap = new TileMap(tileMapData, new BoltTileMapRenderer(BaseTexture.fromImage("assets/tilesheets/main.png"), 64, true, false));
 
-		addNode(graphics);
+		addNode(tilemap);
+
+		var player = Sprite.fromUrl("assets/sprites/player.png");
+
+		addNode(player);
 	}
 
 	override public function update(deltaTime:Float):Void
